b6791c33cadc8d8eacce65743d11b687862e52f3
Darryl <darryl@racecar.com>
2025-07-09 09:25:08 -0400

    feat: Add DataPersistance class

---
+++ DataPersistance.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class DataPersistance {
+    // DARRYL LECRAW
+
+    public boolean hasCsv() {
+
+        return false;
+    }
+
+    public ArrayList<Game> loadGamesFromCsv() {
+
+        // !!!! - CAN BE A JSON
+        //
+        
+        var retArr = new ArrayList<Game>();
+
+        return retArr;
+    }
+
+    public boolean saveGamesToCsv() {
+
+        // LOGIC
+
+        var saveSuccess = true;
+
+        return saveSuccess;
+    }
+}
